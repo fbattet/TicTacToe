9ab5ae79b360e4ee295f7734dde556d618bcb2a2
fbattet <fbattet@gmail.com>
2017-02-01 11:34:30 +0100

    Initial commit and push.

---
+++ app/src/main/java/com/example/fred/tictactoe/model/Cell.java
@@ -0,0 +1,17 @@
+package com.example.fred.tictactoe.model;
+
+/**
+ * Created by fred on 01/02/17.
+ */
+
+public class Cell {
+    private Player value;
+
+    public Player getValue() {
+        return value;
+    }
+
+    public void setValue(Player value) {
+        this.value = value;
+    }
+}
---
+++ app/src/main/java/com/example/fred/tictactoe/model/Player.java
@@ -0,0 +1,8 @@
+package com.example.fred.tictactoe.model;
+
+/**
+ * Created by fred on 01/02/17.
+ */
+
+public enum Player { X, O }
+
